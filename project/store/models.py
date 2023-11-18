6866d68afb7b56f40553650830e6680af610ed68
Stanislav Syromolotov <syromolotov2013@mail.ru>
2023-11-19 01:11:27 +0300

    feat: add model phone to migrate

--- project/store/models.py
+++ project/store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Phone(models.Model):
+    APPLE = 'AP'
+    ANDROID = 'AD'
+
+    MANUFACTURER = [
+        (APPLE, 'Apple'),
+        (ANDROID, 'Android'),
+    ]
+
+    SMARTPHONE = 'SM'
+    MOBILE_PHONE = 'MF'
+
+    TYPE = [
+        (SMARTPHONE, 'smartphone'),
+        (MOBILE_PHONE, 'mobile_phone'),
+    ]
+
+    IOS = 'iOS'
+    SYSTEM = [
+        (IOS, 'iOS'),
+        (ANDROID, 'Android'),
+    ]
+
+    manufacturer = models.CharField(max_length=2, choices=MANUFACTURER, default=APPLE)
+    type = models.CharField(max_length=2, choices=TYPE, default=SMARTPHONE)
+    system = models.CharField(max_length=7, choices=SYSTEM, default=IOS)
+    price = models.IntegerField(default=0)
