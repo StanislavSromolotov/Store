fc324ebc0447e12b48ceefbc08d31d20a11d8840
Stanislav Syromolotov <syromolotov2013@mail.ru>
2023-11-28 14:24:51 +0300

    chore: add table orders

--- project/store/models.py
+++ project/store/models.py
@@ -12,3 +12,13 @@ class Customer(models.Model):
     def __str__(self):
         return f'Customer: {self.name} {self.phone}'
     
+class Orders(models.Model):
+
+    id = models.SmallAutoField(primary_key=True, unique=True)
+    date = models.DateTimeField(blank=False)
+    sum_total = models.IntegerField(blank=False)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.customer_id} {self.sum_total} {self.date}'
+    
