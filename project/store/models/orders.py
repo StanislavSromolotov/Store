f1cdb459d95a0d63577fa17d6a2c8ab87604285b
Stanislav Syromolotov <syromolotov2013@mail.ru>
2023-11-29 17:37:24 +0300

    fix: separate models

---
+++ project/store/models/orders.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from .customer import Customer
+
+
+class Orders(models.Model):
+
+    id = models.SmallAutoField(primary_key=True, unique=True)
+    date = models.DateTimeField(blank=False)
+    sum_total = models.IntegerField(blank=False)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = "store"
+
+    def __str__(self) -> str:
+        return f'{self.customer_id} {self.sum_total} {self.date}'
