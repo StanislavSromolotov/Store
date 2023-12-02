8114a8939bb015217c3bf0b0899ea1b46b047bfb
Stanislav Syromolotov <syromolotov2013@mail.ru>
2023-12-03 01:40:15 +0300

    chore: add many to many fields to orders

--- project/store/models/orders.py
+++ project/store/models/orders.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 from .customer import Customer
-
+from .phone import Phone
+from .phone_case import Phone_case
 
 class Orders(models.Model):
 
@@ -9,6 +10,8 @@ class Orders(models.Model):
     date = models.DateTimeField(blank=False)
     sum_total = models.IntegerField(blank=False)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    phones = models.ManyToManyField(Phone)
+    phone_cases = models.ManyToManyField(Phone_case)
 
     def __str__(self) -> str:
         return f'{self.customer_id} {self.sum_total} {self.date}'
