a8f4a8693eb9a91d5293f12b2e18a2e2ebc8b447
Stanislav Syromolotov <syromolotov2013@mail.ru>
2023-12-03 18:19:19 +0300

    chore: formatted text

--- project/store/migrations/0004_orders.py
+++ project/store/migrations/0004_orders.py
@@ -5,21 +5,30 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('store', '0003_phone_case'),
+        ("store", "0003_phone_case"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Orders',
+            name="Orders",
             fields=[
-                ('id', models.SmallAutoField(primary_key=True, serialize=False, unique=True)),
-                ('date', models.DateTimeField()),
-                ('sum_total', models.IntegerField()),
-                ('customer_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='store.customer')),
-                ('phone_cases', models.ManyToManyField(to='store.phone_case')),
-                ('phones', models.ManyToManyField(to='store.phone')),
+                (
+                    "id",
+                    models.SmallAutoField(
+                        primary_key=True, serialize=False, unique=True
+                    ),
+                ),
+                ("date", models.DateTimeField()),
+                ("sum_total", models.IntegerField()),
+                (
+                    "customer_id",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="store.customer"
+                    ),
+                ),
+                ("phone_cases", models.ManyToManyField(to="store.phone_case")),
+                ("phones", models.ManyToManyField(to="store.phone")),
             ],
         ),
     ]
--- project/store/models/customer.py
+++ project/store/models/customer.py
@@ -2,24 +2,17 @@ from django.db import models
 
 
 class Customer(models.Model):
-
     id = models.SmallAutoField(primary_key=True, unique=True)
     name = models.CharField(
-        max_length=30,
-        blank=False,
-        help_text='Введите имя пользователя')
+        max_length=30, blank=False, help_text="Введите имя пользователя"
+    )
     phone = models.CharField(
-        max_length=12,
-        blank=False,
-        unique=True,
-        help_text='Введите номер телефона')
-    address = models.CharField(
-        max_length=30,
-        help_text='Введите адрес проживания')
+        max_length=12, blank=False, unique=True, help_text="Введите номер телефона"
+    )
+    address = models.CharField(max_length=30, help_text="Введите адрес проживания")
     email = models.CharField(
-        max_length=30,
-        unique=True,
-        help_text='Введите электронную почту')
+        max_length=30, unique=True, help_text="Введите электронную почту"
+    )
 
     def __str__(self):
-        return f'Customer: {self.name} {self.phone}'
+        return f"Customer: {self.name} {self.phone}"
