fd9f0374baa8a2152639ff434e86643a93bdb9db
Stanislav <74504133+StanislavSromolotov@users.noreply.github.com>
2024-04-04 21:13:21 +0300

    chore: change templates (#32)

--- project/store/views.py
+++ project/store/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+# from django.http import HttpResponse, HttpResponseNotFound
+# from django.shortcuts import render
 from django.template.response import TemplateResponse
-from store.models.customer import Customer
+
+# from store.models.customer import Customer
 from store.models.orders import Orders
 from store.models.phone import Phone
 from store.models.phone_case import Phone_case
