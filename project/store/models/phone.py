f80afd440a57fc37fb0e3e725a6ef2e2d12c25bb
Stanislav <74504133+StanislavSromolotov@users.noreply.github.com>
2024-06-12 23:46:42 +0300

    Fix migrations (#35)
    * chore: add line image to db (phone & phone case)
    
    * chore: add discount and description to phonr and phone case

--- project/store/models/phone.py
+++ project/store/models/phone.py
@@ -18,44 +18,52 @@ class Phone(models.Model):
     os = models.CharField(max_length=30, blank=False, choices=PHONE_OS)
     version_os = models.CharField(max_length=30, blank=False)
     phone_model = models.CharField(max_length=30, blank=False)
-    cpu = models.CharField(max_length=30, blank=False, help_text="процессор")
+    cpu = models.CharField(
+        max_length=30, blank=False, verbose_name="Процессор"
+    )
     number_cores = models.IntegerField(
-        blank=False, help_text="кол-во ядер поцессора"
+        blank=False, verbose_name="Кол-во ядер поцессора"
     )
     processor_power = models.IntegerField(
-        blank=False, help_text="разрядность процессора"
+        blank=False, verbose_name="Разрядность процессора"
     )
     battery_type = models.CharField(
-        max_length=20, blank=False, help_text="тип аккумулятора"
+        max_length=20, blank=False, verbose_name="Тип аккумулятора"
     )
     battery_capasity = models.IntegerField(
-        blank=False, help_text="емкость аккумулятора"
+        blank=False, verbose_name="Емкость аккумулятора"
     )
     color = models.CharField(
-        max_length=30, blank=False, help_text="цвет модели телефона"
+        max_length=30, blank=False, verbose_name="Цвет модели телефона"
     )
     screen_size = models.CharField(
-        max_length=15, blank=False, help_text="размер экрана"
+        max_length=15, blank=False, verbose_name="Размер экрана"
     )
     camera = models.CharField(
-        max_length=30, blank=False, help_text="кол-во точек матрицы"
+        max_length=30, blank=False, verbose_name="Кол-во точек матрицы"
     )
     number_cameras = models.IntegerField(
-        blank=False, help_text="цвет модели телефона"
+        blank=False, verbose_name="Цвет модели телефона"
     )
     number_sim_cards = models.IntegerField(
-        blank=False, help_text="кол-во SIM-карт"
+        blank=False, verbose_name="Кол-во SIM-карт"
     )
     memory_card_support = models.CharField(
         max_length=3,
         blank=False,
-        help_text="поддержка дополнительной карты памяти",
+        verbose_name="Поддержка дополнительной карты памяти",
     )
     manufacturer = models.CharField(
-        max_length=20, blank=False, help_text="страна производства"
+        max_length=20, blank=False, verbose_name="Страна производства"
     )
-    price = models.IntegerField(blank=False, help_text="стоимость телефона")
+    price = models.IntegerField(blank=False, verbose_name="Стоимость")
     release_date = models.DateField(blank=False)
+    discount = models.DecimalField(
+        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в %"
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание"
+    )
 
     def __str__(self) -> str:
         return f"{self.phone_model} {self.color} {self.price}"
--- project/store/models/phone_case.py
+++ project/store/models/phone_case.py
@@ -4,18 +4,27 @@ from django.db import models
 class Phone_case(models.Model):
     id = models.SmallAutoField(primary_key=True, unique=True)
     image = models.ImageField(
-        upload_to="store_images", blank=True, null=True, verbose_name="Image"
+        upload_to="store_images",
+        blank=True,
+        null=True,
+        verbose_name="Картинка",
     )
     model = models.CharField(
-        max_length=30, blank=False, help_text="модель чехла для телефона"
+        max_length=30, blank=False, verbose_name="Модель чехла для телефона"
     )
     color = models.CharField(
-        max_length=30, blank=False, help_text="цвет чехла"
+        max_length=30, blank=False, verbose_name="Цвет чехла"
     )
     material = models.CharField(
-        max_length=30, blank=False, help_text="материал чехла"
+        max_length=30, blank=False, verbose_name="Материал чехла"
     )
     price = models.IntegerField(blank=False)
+    discount = models.DecimalField(
+        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в %"
+    )
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание"
+    )
 
     def __str__(self) -> str:
         return f"{self.model} {self.color} {self.material} {self.price}"
